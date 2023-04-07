61ff9f6d69d6c1f2d21cda79affadb3e5f88cc23
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-04-08 01:25:48 +0300

    Character movement created

---
+++ GOUA-Gamejam/Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private int movementSpeed = 10;
+    Vector2 horizontalMovement;
+    Vector2 verticalMovement;
+    
+
+    void Start()
+    {
+        horizontalMovement = new Vector2(movementSpeed * Input.GetAxisRaw("Horizontal") * Time.deltaTime , 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(movementSpeed * Input.GetAxisRaw("Horizontal") * Time.deltaTime , movementSpeed * Input.GetAxisRaw("Vertical") * Time.deltaTime ));
+        
+    }
+}
