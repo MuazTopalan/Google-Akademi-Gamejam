c239f3006589fb3748a6b7224635a30ae478c30e
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-04-08 19:26:24 +0300

    Damage

--- GOUA-Gamejam/Assets/Scripts/Character/Projectile.cs
+++ GOUA-Gamejam/Assets/Scripts/Character/Projectile.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+
+    public static int projectileDamage = 30;
     private void OnCollisionEnter2D(Collision2D other) {
         Destroy(gameObject);
     }
--- GOUA-Gamejam/Assets/Scripts/PassiveSkills/Bible/Rotate.cs
+++ GOUA-Gamejam/Assets/Scripts/PassiveSkills/Bible/Rotate.cs
@@ -8,6 +8,7 @@ public class Rotate : MonoBehaviour
     public Transform target;
     public float orbitDistance = 5.0f;
     public float orbitDegreesPerSec = 180.0f;
+    public static int bibleDamage  = 30;
     void Orbit()
     {
         transform.position = target.position + (transform.position - target.position).normalized * orbitDistance;
