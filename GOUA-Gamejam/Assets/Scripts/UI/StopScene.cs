da8bb449f088041fc29d5f445d0a284f5a464737
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-04-10 05:50:25 +0300

    Last Adjustments

--- GOUA-Gamejam/Assets/Scripts/UI/StopScene.cs
+++ GOUA-Gamejam/Assets/Scripts/UI/StopScene.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class StopScene : MonoBehaviour
 {
     [SerializeField] private GameObject Stop;
+    [SerializeField] private GameObject doubleBible;
+    [SerializeField] private GameObject tripleBible;
     
 
     
     void Update()
     {
         if(Input.GetKey(KeyCode.E) == true){
+            Time.timeScale = 0;
             Debug.Log("Deneme");
             Stop.SetActive(true);
+            doubleBible.SetActive(false);
+            tripleBible.SetActive(true);
+        }
+        if(Input.GetKey(KeyCode.Q) == true){
+            Time.timeScale = 1;
+            Debug.Log("Deneme");
+            Stop.SetActive(false);
+            doubleBible.SetActive(false);
+            tripleBible.SetActive(true);
         }
         
     }
